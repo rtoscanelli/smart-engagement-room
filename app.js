551a0d4053aedc7f982ef4ba26bb2374835e3122
Ricardo Toscanelli <ricardotoscanelli@gmail.com>
2024-03-12 17:42:28 +0000

    Update dashboard information

--- app.js
+++ app.js
@@ -9,64 +9,32 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-let clients = [];
-let facts = [];
-
-// To get the number of clients attached
-app.get("/status", (_, res) => {
-    res.json({ clients: clients.length });
-});
-
-// To send events to all clients
-function eventsHandler(req, res, next) {
-    const headers = {
-        "Content-Type": "text/event-stream",
-        Connection: "keep-alive",
-        "Cache-Control": "no-cache",
-    };
-    res.writeHead(200, headers);
-    const data = `data: ${JSON.stringify(facts)}\n\n`;
-    res.write(data);
-
-    const clientId = Date.now();
-    const newClient = {
-        id: clientId,
-        res,
-    };
-    clients.push(newClient);
-
-    req.on("close", () => {
-        console.log(`${clientId} Connection closed`);
-        clients = clients.filter((client) => client.id !== clientId);
-    });
-}
-
-app.get("/events", eventsHandler);
-
-function sendEventsToAll(newFact) {
-    clients.forEach((client) =>
-        client.res.write(`data: ${JSON.stringify(newFact)}\n\n`),
-    );
-}
+let data = [];
+let currentNumber = 0;
 
 async function addData(req, res, next) {
-    const newFact = req.body;
-    facts.push(newFact);
-    res.json(newFact);
-    return sendEventsToAll(newFact);
+	console.log("Received data", req.body);
+	const newData = req.body;
+	data.push(newData);
+    currentNumber = newData.totalNumber;
+	res.json(newData);
 }
+
+app.get("/number", (_, res) => {
+    res.json({ totalNumber: currentNumber });
+});
 
 app.post("/data", addData);
 
 app.get("/", (_, res) => {
-    res.sendFile(path.join(__dirname, "views", "index.html"));
+	res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 app.get("/classroom-lights.html", (_, res) => {
-    res.sendFile(path.join(__dirname, "views", "classroom-lights.html"));
+	res.sendFile(path.join(__dirname, "views", "classroom-lights.html"));
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,31 +1,106 @@
-let eventSource;
+let currentStudents = 0;
+let presentStudentsHistory = [];
+let maxStudents = 0;
+let maxStudentsHistory = [];
+let averageStudents = 0;
+let averageStudentsHistory = [];
+let labels = [];
 
-function addFact(data) {
-    if (data.info == undefined) {
-        totalStudents = 0;
-    } else {
-        totalStudents = data.info;
-        const totalStudentsCard = document.getElementById("total-students");
-        totalStudentsCard.classList.add("animation");
-        document.getElementById("total-students-number").innerText = totalStudents;
-        setTimeout(() => {
-            totalStudentsCard.classList.remove("animation");
-        }, 300);
+function updateStatistics(data) {
+    if (data.totalNumber === currentStudents || data.totalNumber === undefined) {
+        console.log("No new data");
+        return;
     }
-
+    currentStudents = data.totalNumber ? data.totalNumber : 0;
+    presentStudentsHistory.push(currentStudents);
+    maxStudents = Math.max(maxStudents, currentStudents);
+    maxStudentsHistory.push(maxStudents);
+    averageStudents = Math.round(
+        (averageStudents * (presentStudentsHistory.length - 1) + currentStudents) /
+        presentStudentsHistory.length,
+    );
+    averageStudentsHistory.push(averageStudents);
+    const presentStudentsCard = document.getElementById("present-students");
+    presentStudentsCard.classList.add("animation");
+    document.getElementById("present-students-number").innerText =
+        currentStudents;
+    const maxStudentsCard = document.getElementById("max-students");
+    if (maxStudents == currentStudents) {
+        maxStudentsCard.classList.add("animation");
+    }
+    document.getElementById("max-students-number").innerText = maxStudents;
+    const averageStudentsCard = document.getElementById("average-students");
+    averageStudentsCard.classList.add("animation");
+    document.getElementById("average-students-number").innerText =
+        averageStudents;
+    setTimeout(() => {
+        presentStudentsCard.classList.remove("animation");
+        maxStudentsCard.classList.remove("animation");
+        averageStudentsCard.classList.remove("animation");
+    }, 300);
+    labels.push(new Date().toLocaleTimeString());
+    myChart.update();
 }
 
 function startListening() {
-    if (!eventSource) {
-        console.log("Starting to listen for events");
-        eventSource = new EventSource("/events");
-
-        eventSource.onmessage = (event) => {
-            console.log("Received event", event);
-            const data = JSON.parse(event.data);
-            addFact(data);
-        };
-    }
+    fetch("/number")
+        .then((res) => res.json())
+        .then((data) => {
+            updateStatistics(data);
+        });
 }
 
-startListening();
+const ctx = document.getElementById("chart").getContext("2d");
+const data = {
+    labels: labels,
+    datasets: [
+        {
+            label: "Max Students",
+            data: maxStudentsHistory,
+            fill: false,
+            backgroundColor: "rgba(7, 176, 255, 0.2)",
+            borderColor: "rgba(7, 176, 255, 1)",
+            pointRadius: 5,
+            pointHoverRadius: 8,
+            pointHitRadius: 5,
+        },
+        {
+            label: "Present Students",
+            data: presentStudentsHistory,
+            fill: false,
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            pointRadius: 5,
+            pointHoverRadius: 8,
+            pointHitRadius: 5,
+        },
+        {
+            label: "Average Students",
+            data: averageStudentsHistory,
+            fill: false,
+            backgroundColor: "rgba(0, 184, 148, 0.2)",
+            borderColor: "rgba(0, 184, 148, 1)",
+            pointRadius: 5,
+            pointHoverRadius: 8,
+            pointHitRadius: 5,
+        },
+    ],
+};
+
+const myChart = new Chart(ctx, {
+    type: "line",
+    data: data,
+    options: {
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true,
+                    },
+                },
+            ],
+        },
+    },
+});
+
+setInterval(startListening, 2000);
