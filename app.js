f5c0cc2de047cfc470e721ac6e97f8e245811005
Ricardo Toscanelli <ricardotoscanelli@gmail.com>
2024-03-19 16:57:41 +0000

    Add none same site cookie option

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(session({
+    cookie: {
+        sameSite: 'none',
+        secure: 'auto',
+    },
     secret: 'secret',
     resave: false,
     saveUninitialized: true,
